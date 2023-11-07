0fea61204d54f52e5c10d5a9197060841418f58c
Alex <alex@example.com>
2023-11-07 12:25:15 +0100

    combine lookup and meta tests into a single bash script

--- test/meta.sh
+++ test/meta.sh
@@ -8,3 +8,5 @@ test_code=EV_KEY
 actual_value=$($interpreter $(dirname $0)/lookup.lua $@ $test_code)
 echo got $actual_value for $test_code
 [ 1 = "$actual_value" ]
+
+$interpreter $(dirname $0)/meta.lua $@

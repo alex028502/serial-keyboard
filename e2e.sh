554eb996ba9e50110c150c8fa1fa815f93fc07f1
Alex <alex@example.com>
2023-11-08 20:26:02 +0100

    remove cool scheme with a fifo connected to a socat tty
    that was there to shut down nicely - but the nice shutdown is already
    tested by the driver tests using just fifo - here we can end with an
    error
    
    and don't wait so long for socat tty to start

--- e2e.sh
+++ e2e.sh
@@ -32,10 +32,6 @@ echo
 
 echo
 echo ---- TEST E2E -----------
-# two way pty is still needed for this test because the fake arduino will open
-# the serial path for read and write - and the driver will also want to open
-# for read - so a single path won't work
-mkfifo $dev/serial_connector
 echo ---- OPEN SERIAL --------
 socat -d -d pty,raw,echo=0,link=$dev/serial pty,raw,echo=0,link=$dev/serial.interface &
 remember serial $!
@@ -46,37 +42,45 @@ echo
 # setting for the socat pty that will make it close nicely like the fifo
 # but maybe if there is a way, somebody will answer someday
 # https://unix.stackexchange.com/questions/760016
-sleep 1
-cat > $dev/serial_connector < $dev/serial &
-cat_pid=$!
-remember cat $cat_pid
-sleep .1
-kill -0 $cat_pid
+# for now I am using socat to test an error here and using a fifo in the
+# driver tests to simulate a clean top where the input fd just ends
 echo ---- start driver -------
-$driver_script $driver_lib $dev/serial_connector $dev/uinput &
+$driver_script $driver_lib $dev/serial $dev/uinput &
 driver_id=$!
 remember driver2 $driver_id
 echo see if it started:
 kill -0 $driver_id
 echo ---- test driver init ---
-read stty_line < $dev/serial_connector
+read stty_line < $dev/serial.interface
 echo STTY: $stty_line
 baud=$(echo $stty_line | xargs -n1 echo | grep -E '^[0-9]+$')
 echo BAUD: $baud
 # the serial port should not affect anything here but might need to exist
-$interpreter $(dirname $0)/driver/test/init.lua $helper $library $dev/uinput $dev/serial_connector
+$interpreter $(dirname $0)/driver/test/init.lua $helper $library $dev/uinput $dev/serial.interface
 echo ---- test all -----------
 $interpreter $(dirname $0)/misc/e2e.lua $(dirname $0)/driver/test/lib.lua $firmware $helper $library $firmware_test_lua_lib $dev/serial $dev/serial.interface $dev/uinput $baud
-kill -0 $driver_id
-# we didn't save the id in a variable so we can look it up in the process list
 socat_pid=$(echo $processes | xargs -n2 echo | grep -w serial | awk '{ print $2 }')
 kill $socat_pid
+# there are really at least three ways of shutting down that ought to be tested
+# - ending serial nicely (tested with fifo in driver tests)
+# - ending serial not nicely (tested here)
+# - SIGINT and/or SIGTERM to the driver program
+# the third one is not tested at all but manually I do it all the time when
+# trying this out - and it seems to work. one problem is that I haven't managed
+# to measure test coverage that way.
+timer=""
 while kill -0 $driver_id
 do
     timer="x$timer"
     c="$(echo $timer | wc -c)"
     # maxium two seconds
-    # that was the sleep before but it seems to need about 11 iterations
+    # this took like eleven iterations when socat was connected to a fifo
+    # and turning off socat caused the fifo to end nicely - but now that the
+    # socat pty is connected directly to the driver, turning off socat causes
+    # an almost immediate error - so this only has one iteration but also
+    # doesn't seem to be immediate enough that this doesn't run (from coverage)
+    # so it is still necessary because cleaning up the driver with kill would
+    # mess up coverage
     echo waiting for driver to shut down $c
     [ "$c" != 20 ]
     sleep 0.1
